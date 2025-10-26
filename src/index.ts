b7853adde7546054023a279a8f36f9457d2dc357
n43-3m <naeemur006@gmail.com>
2025-10-27 03:11:12 +0600

    add biomeJs

--- src/index.ts
+++ src/index.ts
@@ -4,12 +4,16 @@ import { Hono } from 'hono'
 const app = new Hono()
 
 app.get('/', (c) => {
-  return c.text('Hello Hono!')
+	return c.text('Hello Hono!')
 })
 
-serve({
-  fetch: app.fetch,
-  port: 3000
-}, (info) => {
-  console.log(`Server is running on http://localhost:${info.port}`)
-})
+serve(
+	{
+		fetch: app.fetch,
+		port: 3000,
+	},
+
+	(info) => {
+		console.log(`Server is running on http://localhost:${info.port}`)
+	}
+)
